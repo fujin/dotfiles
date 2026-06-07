2ea4339364e126507ed12c570739664037153cd9
AJ Christensen <achristensen@nvidia.com>
2026-06-08 10:16:56 +1200

    feat: update shell and tooling defaults

--- dot_config/private_fish/private_config.fish
+++ dot_config/private_fish/private_config.fish
@@ -1,15 +1,33 @@
-if command -sq mise
-    if status is-interactive
-        mise activate fish | source
-    else
-        mise activate fish --shims | source
-    end
+set -l open_files_limit 65536
+set -l current_open_files_limit (ulimit -n)
+if test "$current_open_files_limit" != "unlimited"; and test "$current_open_files_limit" -lt "$open_files_limit"
+    ulimit -n $open_files_limit 2>/dev/null
+end
+
+if test -d "$HOME/.local/bin"
+    fish_add_path --prepend --move "$HOME/.local/bin"
+end
+
+set --export MISE_GITHUB_USE_GIT_CREDENTIALS 1
+set -l mise_bin
+if test -x "$HOME/.local/bin/mise"
+    set mise_bin "$HOME/.local/bin/mise"
+else if command -sq mise
+    set mise_bin (command -s mise)
+end
+
+if test -n "$mise_bin"
+    $mise_bin activate fish | source
 end
 
 if test -x /opt/homebrew/bin/brew
     /opt/homebrew/bin/brew shellenv | source
 else if test -x /usr/local/bin/brew
     /usr/local/bin/brew shellenv | source
+end
+
+if test -d "$HOME/.local/bin"
+    fish_add_path --prepend --move "$HOME/.local/bin"
 end
 
 if status is-interactive
@@ -42,11 +60,15 @@ if status is-interactive
 end
 
 export EDITOR="nvim"
+function codex --wraps codex
+    set -lx CODEX_GITHUB_MCP_BEARER_TOKEN (gh auth token)
+    command codex $argv
+end
+
 # The next line updates PATH for the Google Cloud SDK.
 if test -f "$HOME/google-cloud-sdk/path.fish.inc"
     . "$HOME/google-cloud-sdk/path.fish.inc"
 end
-export PATH="$HOME/.local/bin:$PATH"
 
 if test -d "$HOME/.bun"
     set --export BUN_INSTALL "$HOME/.bun"
